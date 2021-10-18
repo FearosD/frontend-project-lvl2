6aa708f16a96dbdf25da4d492975172a12166587
FearosD <fearos@valar.ru>
2021-10-18 19:57:02 +0300

    add commander.js & create Makefile

--- Makefile
+++ Makefile
@@ -2,7 +2,7 @@ install:
 	npm ci
 
 gendiff:
-	node bin/gendiff.js
+	node --experimental-vm-modules bin/gendiff.js
 
 publish:
 	npm publish --dry-run
--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,3 +1,6 @@
 #!/usr/bin/env node
-
-console.log('test');
+import { Command } from "commander/esm.mjs";
+const program = new Command();
+program.description('Compares two configuration files and shows a difference.');
+program.version('0.0.1', '-v, --version', 'output the version number');
+program.parse();
