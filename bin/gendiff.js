22825d960b4a555e3aea00f8649ec4e1b50681f3
FearosD <fearos@valar.ru>
2021-10-18 20:09:46 +0300

    add linter

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
-import { Command } from "commander/esm.mjs";
+import { Command } from 'commander/esm.mjs';
+
 const program = new Command();
 program.description('Compares two configuration files and shows a difference.');
 program.version('0.0.1', '-v, --version', 'output the version number');
