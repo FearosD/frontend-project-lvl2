9537f792a5e31a01748ce3c63b9c87feb29ae743
FearosD <fearos@valar.ru>
2021-10-19 13:04:10 +0300

    add fixtures & main index.js

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,9 +1,11 @@
 #!/usr/bin/env node
 import { Command } from 'commander/esm.mjs';
+import gendiff from '../src/index.js';
 
 const program = new Command();
 program.description('Compares two configuration files and shows a difference.');
 program.version('0.0.1', '-v, --version', 'output the version number');
 program.option('-f, --format [type]', 'output format');
 program.arguments('<filepath1> <filepath2>');
+program.action((filepath1, filepath2) => console.log(gendiff(filepath1, filepath2)));
 program.parse();
---
+++ src/index.js
@@ -0,0 +1,3 @@
+const gendiff = (filepath1, filepath2) => `File-1 ${filepath1}\nFile-2 ${filepath2}`;
+
+export default gendiff;
